0a029b8df1e10a789b6b562a7cd5132c0e02ea75
cgar246 <christiang3268@yahoo.com>
2022-01-11 21:22:10 -0800

    created a js file for the page to generate

--- test/test-engineer.js
+++
@@ -1,9 +0,0 @@
-const engineer = require("../lib/Engineer")
-test('new engineer name test', () =>{
-    const newEngineer = new Engineer (1,"name","email", "github");
-    expect(newEngineer.id).toBeGreaterThan(0);
-    expect(newEngineer.name).toBe('Elo');
-    expect(newEngineer.email).toBe('elo@mail.com');
-    expect(newEngineer.gitHub).toBe('zelo');
-    expect(newEngineer.getRole()).toBe('Engineer');
-});
--- test/test-manager.js
+++
@@ -1,9 +0,0 @@
-const manager = require("../lib/Manager");
-test('Creates a Manager object', () => {
-    const miManager = new Manager (1,"name","email",70);
-    expect(miManager.id).toBeGreaterThan(0);
-    expect(miManager.name).toBe('Tony');
-    expect(miManager.email).toBe('zapple@mail.com');
-    expect(miManager.officeNumber).toBeGreaterThan(0);
-    expect(miManager.getRole()).toBe('Manager');
-});
